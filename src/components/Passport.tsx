08643fc82cce303f60aa93e0147cc53f9ae362c7
Kauan Felipe <kauan_falima@hotmail.com>
2024-08-17 13:12:35 -0300

    update front and page1

--- src/components/Passport.tsx
+++ src/components/Passport.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import brasao from "../assets/brasaorprp.png";
+import aguia from "../assets/aguia.png";
 import { TbEPassport } from "react-icons/tb";
 
 const Passport: React.FC = () => {
@@ -13,17 +14,19 @@ const Passport: React.FC = () => {
         <>
         <div className={`book ${isOpen ? "open-book" : "closed-book"}`} onClick={handleClick}>
             <div className={`back ${isOpen ? "open-back" : "closed-back"}`}></div>
-            <div className={`page6 ${isOpen ? "open-page6" : "closed-page6"}`}></div>
+            <div className={`page6 ${isOpen ? "open-page6" : "closed-page6"}`}>
+            <div className="page6-content">
+                </div>
+            </div>
             <div className={`page4 ${isOpen ? "open-page4" : "closed-page4"}`}></div>
             <div className={`page2 ${isOpen ? "open-page2" : "closed-page2"}`}></div>
             <div className={`page1 ${isOpen ? "open-page1" : "closed-page1"}`}>
-                <div className="pagina">
-                    <h1>TESTESTE</h1>
+            <div className="page1-content">
                 </div>
             </div>
             <div className={`front ${isOpen ? "open-front" : "closed-front"}`}>
-                <h1>Passaporte</h1>
-                <img src={brasao} width={140} />
+                <h1>Passporte</h1>
+                <img src={brasao} width={150} />
                 <h2>Ribeirão Pires</h2>
                 <TbEPassport className="icon" />
             </div>
