45a6e2a68f9fbadb715728882c0068eae1e13db3
kauanfelipe96 <kauan_falima@hotmail.com>
2024-08-21 17:15:47 -0300

    updating signature and avatar

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface IPassportData {
+  citizenid: string;
+  firstname: string;
+  lastname: string;
+  birthdate: number;
+  nationality: string;
+  gender: number;
+  city: string;
+  color: string;
+}
